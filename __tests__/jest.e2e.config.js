9048efc3060db20e7199ce409022c55c1bd652a7
Jas <azoulayjason@gmail.com>
2025-04-27 16:29:25 +0200

    Update test scripts in package.json to run unit, integration, and end-to-end tests sequentially. Add setup file path to end-to-end Jest configuration for improved test environment setup.

--- __tests__/jest.e2e.config.js
+++ __tests__/jest.e2e.config.js
@@ -6,4 +6,5 @@ module.exports = {
   testMatch: ['**/__tests__/e2e/**/*.test.ts'],
   // Timeout très long pour les tests end-to-end
   testTimeout: 60000,
+  setupFilesAfterEnv: ['<rootDir>/setup.ts'], // Explicitly set the path relative to __tests__
 }; 
